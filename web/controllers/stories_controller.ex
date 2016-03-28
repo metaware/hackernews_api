4023a1b453b8a379f0989b221bc75dc54681b900
kmptkp <kmptkp@gmx.com>
2016-03-28 12:46:20 +0200

    Generalise frontpage API to active stories, best stories, newest, ask-hn, show-hn

--- web/controllers/stories_controller.ex
+++ web/controllers/stories_controller.ex
@@ -1,12 +1,19 @@
 defmodule HackernewsApi.StoriesController do
   use HackernewsApi.Web, :controller
 
-  def top_stories(conn, _params) do
+  def top_stories(conn, _params),    do: stories("", conn)
+  def active_stories(conn, _params), do: stories("active", conn)
+  def newest_stories(conn, _params), do: stories("newest", conn)
+  def best_stories(conn, _params),   do: stories("best", conn)
+  def show_hn(conn, _params),        do: stories("show", conn)
+  def ask_hn(conn, _params),         do: stories("ask", conn)
+
+
+
+  defp stories(path, conn) do
     case Hackernews.Frontpage.fetch do
-      {:ok, response} ->
-        json(conn, response)
-      {:error, reason} ->
-        json(conn, reason)
+      {:ok, response}  -> json(conn, response)
+      {:error, reason} -> json(conn, reason)
     end
   end
 
--- web/router.ex
+++ web/router.ex
@@ -15,7 +15,7 @@ defmodule HackernewsApi.Router do
 
   scope "/", HackernewsApi do
     pipe_through :browser
-    
+
     get "/", DefaultController, :index
   end
 
@@ -23,6 +23,12 @@ defmodule HackernewsApi.Router do
   scope "/api", HackernewsApi do
     pipe_through :api
 
-    get "/top-stories", StoriesController, :top_stories
+    get "/top-stories",   StoriesController, :top_stories
+    get "/active-stories", StoriesController, :active_stories
+    get "/newest-stories", StoriesController, :newest_stories
+    get "/best-stories",  StoriesController, :best_stories
+    get "/show-hn",       StoriesController, :show_hn
+    get "/ask-hn",        StoriesController, :ask_hn
+
   end
 end
